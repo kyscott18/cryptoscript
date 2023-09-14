c2bdc2e7a6acbb327048d2948ac4a1227c910b11
Kyle Scott <kyscott@umich.edu>
2023-09-13 21:03:10 -0400

    .

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  name: "reverse-mirage",
+  name: "cryptoscript",
   entry: ["src/index.ts"],
   outDir: "dist",
   format: ["esm", "cjs"],
